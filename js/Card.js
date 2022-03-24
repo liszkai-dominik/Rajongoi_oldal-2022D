067c354ee56812ad6c6bb8e56ac94a454f212ca6
Luo-Hong-Jun <luo.hong.jun@students.jedlik.eu>
2022-03-24 16:48:07 +0100

    sussy

--- js/Card.js
+++ js/Card.js
@@ -1,3 +1,34 @@
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+//nyitó gomb
+var btn = document.querySelectorAll("button.modal-button");
+
+// általános modal
+var modals = document.querySelectorAll('.modal');
+
+//zár
+var spans = document.getElementsByClassName("close");
+
+// nyitásra
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+//bezárás
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+//Bezárás (kinti)
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
